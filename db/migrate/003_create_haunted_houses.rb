d279101e781b2e63ca86e422dc8cf2cdc0c04c94
Rabah <rmerzoug471@gmail.com>
2022-08-12 14:53:27 -0500

    commit tree 974b39b8a2be532e8dc1303f120c831e5fe9e2d2

---
+++ db/migrate/003_create_haunted_houses.rb
@@ -0,0 +1,19 @@
+class CreateHauntedHouses < ActiveRecord::Migration[6.1]
+    def change
+        create_table :haunted_houses do |t|
+          t.string :name
+          t.string :location
+          t.string :theme
+          t.float :price
+          t.boolean :family_friendly
+          t.datetime :opening_time
+          t.datetime :closing_time
+          t.text :description
+        end
+    end
+end
+
+
+opening_date (datetime)
+closing_date (datetime)
+description (text)
